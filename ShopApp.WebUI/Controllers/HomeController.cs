993060b4cdb1e1b3d1e8acda917f9be025999029
can.gezgin <can.gezgin@WRPNW064>
2023-05-31 11:32:09 +0300

    Test verileri eklenmiştir.

--- ShopApp.WebUI/Controllers/HomeController.cs
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -20,18 +20,7 @@ namespace ShopApp.WebUI.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy() 
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)] 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(_productService.GetAll());
         }
     }
 }
